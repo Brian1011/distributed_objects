3598d443d68cc45e4854006b937bc57acd628a60
Brian1011 <mutindabrian26@gmail.com>
2019-09-21 17:27:41 +0300

    fill in university model

--- src/main/java/com/client/rest_feign/models/Universities.java
+++ src/main/java/com/client/rest_feign/models/Universities.java
@@ -1,4 +1,50 @@
 package com.client.rest_feign.models;
 
 public class Universities {
+    private Long id;
+    private String name;
+    private String location;
+
+    public Universities() {
+
+    }
+
+    public Universities(Long id, String name, String location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Universities{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
 }
